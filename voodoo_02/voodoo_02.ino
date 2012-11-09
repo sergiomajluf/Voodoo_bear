3ff02510375ba2e276384632403202dea98e846b
Sergio Majluf <sergio.majluf@gmail.com>
2012-11-09 13:01:45 -0500

    New edits

--- voodoo_02/voodoo_02.ino
+++ voodoo_02/voodoo_02.ino
@@ -3,7 +3,7 @@ const int emiTx = 3;
 
 // buttons inside the hand
 const int rightHand = 4;
-const int lefttHand = 5;
+const int leftHand = 5;
 
 const int flexBodyFront = A0;
 const int flexBodyBack = A1;
@@ -11,8 +11,13 @@ const int flexBodyBack = A1;
 const int sensorLow = 400;
 const int sensorHigh = 600;
 
+const int ledPin = 13;
+
 boolean negativeFeedback = false;
 boolean positiveFeedback = false;
+
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
 
 
 void setup(){
@@ -20,28 +25,40 @@ void setup(){
   pinMode (rightHand, INPUT);
   pinMode (leftHand, INPUT);
 
+  pinMode (ledPin, OUTPUT);
+
   digitalWrite(rightHand, LOW);
-  digitalWrite (lefttHand, LOW);
+  digitalWrite (leftHand, LOW);
 }
 
 void loop(){
 
-  if (digitalRead (rightHand) == HIGH || digitalRead (lefttHand) == HIGH) {
+  //buttonState = digitalRead(rightHand);
+
+
+  if (digitalRead (rightHand) == HIGH || digitalRead (leftHand) == HIGH) {     
+    // When either hand is pressed, do this:    
+    digitalWrite(ledPin, HIGH);  
     positiveFeedback = true;
   } 
+  else {
+    // turn LED off, or whatever else
+    digitalWrite(ledPin, LOW); 
+  }
 
-
-
+  if (positiveFeedback) {
+    // select random phrase, and send it to emic
+  }
 }
 
 
 void isNegative() {
-  if (analogRead(flexBodyFront) > sensorLow || analogRead(flexBodyFront) < SensorHight){
+  if (analogRead(flexBodyFront) > sensorLow || analogRead(flexBodyFront) < sensorHigh){
     // do negative feedback
   } 
 
 
-
-
 }
+
+
 
